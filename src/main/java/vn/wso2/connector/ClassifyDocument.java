dbea59338e6b4a06c3043a549ea3ac0f29eb1797
Tran Quang Minh <minh.tran.1@swisspost.com>
2022-03-01 11:26:49 +0700

    fix bug json result

--- src/main/java/vn/wso2/connector/ClassifyDocument.java
+++ src/main/java/vn/wso2/connector/ClassifyDocument.java
@@ -12,15 +12,24 @@
  */
 package vn.wso2.connector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.synapse.MessageContext;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.connector.core.ConnectException;
 
 import com.amazonaws.services.comprehend.model.ClassifyDocumentRequest;
 import com.amazonaws.services.comprehend.model.ClassifyDocumentResult;
+import com.amazonaws.services.comprehend.model.DocumentClass;
+import com.amazonaws.services.comprehend.model.DocumentLabel;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import junit.framework.Assert;
 
@@ -48,14 +57,32 @@ public class ClassifyDocument extends ComprehendAgent {
             final ClassifyDocumentResult classifyDocumentResult = this.getComprehendClient().classifyDocument(
                 classifyDocumentRequest);
 
-            final String jsonAsString = new Gson().toJson(classifyDocumentResult);
+            final Gson gson = new Gson();
 
-            final JSONObject jsonObject = new JSONObject(jsonAsString);
+            final JsonObject jsonObject = new JsonObject();
+
+            final List<DocumentClass> documentClasses = Optional.ofNullable(classifyDocumentResult.getClasses()).orElse(
+                new ArrayList<>());
+            final JsonElement documentClassElement = gson.toJsonTree(documentClasses,
+                new TypeToken<List<DocumentClass>>() {
+                }.getType());
+            final JsonArray documentClassAsJsonArray = documentClassElement.getAsJsonArray();
+
+            final List<DocumentLabel> documentLabels = Optional.ofNullable(classifyDocumentResult.getLabels()).orElse(
+                new ArrayList<>());
+            final JsonElement documentLabelElement = gson.toJsonTree(documentLabels,
+                new TypeToken<List<DocumentLabel>>() {
+                }.getType());
+            final JsonArray documentLabelAsJsonArray = documentLabelElement.getAsJsonArray();
+
+            jsonObject.add("Classes", documentClassAsJsonArray);
+            jsonObject.add("Labels", documentLabelAsJsonArray);
+
             messageContext.setProperty("classifyDocumentResult", jsonObject);
-
             LOGGER.info("Classify the document and put the response to classifyDocumentResult property. Took {} ms",
                 System.currentTimeMillis() - start);
         } catch (Exception e) {
+            LOGGER.error("", e);
             throw new ConnectException(e, "Error while classifying the document. Detail: ");
         }
     }
